733ff1abd4b544c2fb950c5c28f5ed23d5e7b252
yasu971 <yasuishii1421@gmail.com>
2020-10-30 10:23:33 +0900

    既読機能のエンドポイントを、pathパラメーターを使用して記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 # get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
-  get 'posts', to: 'posts#checked'
+  get 'posts/:id', to: 'posts#checked'
 end
