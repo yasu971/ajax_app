dea549094f4313e972d846adbc512b76ca8ad20a
yasu971 <yasuishii1421@gmail.com>
2020-10-30 16:01:08 +0900

    コントローラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,6 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
-
-  # 削除
-  # def new
-  # end
-
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
