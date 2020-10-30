14bd66e2f4323f87f2e5badb24b9110842e8a327
yasu971 <yasuishii1421@gmail.com>
2020-10-30 15:49:24 +0900

    バイディングプライ削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,6 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
   def checked
-    binding.pry
     post = Post.find(params[:id])
     if post.checked
       post.update(checked: false)
